2d9c3c8e26b5d6de7999e3d6ae0e07017a50f544
Majra <majra.oruc@gmail.com>
2021-03-13 00:51:28 +0100

    ex02 single quotations

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    //Only change code below this line
+    var myLink = '<a href=\"http://www.example.com\" target=\"blank\">Link</a>';
+    //only change code above this line 
+
+    return myLink;
+}
+
+console.log(main());
+module.exports = main;
