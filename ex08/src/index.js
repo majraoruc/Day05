6db32aafea71c596f02c61885c946546a606a7d8
Majra <majra.oruc@gmail.com>
2021-03-13 01:17:23 +0100

    ex08 length property

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    //Only change code below this line
+    lastName = "Doe";
+    var lastNameLength = lastName.length;
+    //only change code above this line 
+    return lastNameLength;
+}
+console.log(main());
+module.exports = main;
