cb925c7e12b7c7a57df8bf680b99b9aa5b156c24
Majra <majra.oruc@gmail.com>
2021-03-13 01:22:15 +0100

    ex10 string immutability

---
+++ ex10/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    //Only change code below this line
+    myString = "Jello World";
+    myString = "Hello World"
+    //only change code above this line 
+    return myString;
+}
+console.log(main());
+module.exports = main;
