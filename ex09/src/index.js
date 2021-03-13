271da38403d9dc0dd09f2cc704fce12bc785b738
Majra <majra.oruc@gmail.com>
2021-03-13 01:19:56 +0100

    ex09 bracket notation

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    //Only change code below this line
+    lastName = "Doe";
+    var firstLetterOfLastName = lastName[0];
+    //only change code above this line 
+    return firstLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
