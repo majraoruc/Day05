8a2b1898635ca037013aab7b3ea0526df5e7acbd
Majra <majra.oruc@gmail.com>
2021-03-13 01:08:50 +0100

    ex05 combining strings

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line
+    var myName = "Majra";
+    var myString = "My name is " + myName + " and I am well!";
+    //only change code above this line 
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
