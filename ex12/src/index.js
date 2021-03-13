0d2c83c96627348db729dda4dba17c36828ba4ed
Majra <majra.oruc@gmail.com>
2021-03-13 01:28:14 +0100

    ex12 last letter manipulations

---
+++ ex12/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    //Only change code below this line
+    lastName = "Tesla";
+    var lastLetterOfLastName = lastName[lastName.length - 1];
+    //only change code above this line 
+    return lastLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
