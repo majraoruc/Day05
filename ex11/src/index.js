c6156e11a8c914d7ea3205902ec3c8752d1ff2ee
Majra <majra.oruc@gmail.com>
2021-03-13 01:24:41 +0100

    ex11 nth character

---
+++ ex11/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    //Only change code below this line
+    lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+    //only change code above this line 
+    return thirdLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
